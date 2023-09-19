40cc7932570a4492d8391b3cdf6093058bb0b0e3
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-09-19 16:19:26 -0400

    index start

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>All boardgames</h1>
+    <div v-for="boardgame in boardgames">
+      <h2>{{ boardgame.name }}</h2>
+      <!-- <img v-bind:src="photo.url" v-bind:alt="photo.name" /> -->
+      <p>Price: {{ boardgame.price }}</p>
+      <p>Number of Players: {{ boardgame.player }}</p>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      boardgames: [],
+    };
+  },
+  created: function() {
+    this.indexBoardgames();
+  },
+  methods: {
+    indexBoardgames: function() {
+      axios.get("/boardgames").then(response => {
+        console.log("boardgames index", response);
+        this.boardgames = response.data;
+      });
+    },
+  },
+};
 </script>
